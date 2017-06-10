6426a0113536208ac2792dc6acfe33e457d7605d
Yevhenii <solembum@me.com>
2017-06-11 01:20:08 +0300

    Profile design

--- AskAbout/wwwroot/js/Shared/Layout.js
+++ AskAbout/wwwroot/js/Shared/Layout.js
@@ -44,4 +44,16 @@ $(document)
             });
     });
 
-$(".menu .browse").popup()
+$(".menu .browse").popup();
+
+// more click
+$('.card-profile-stats-more-link').click(function (e) {
+    e.preventDefault();
+    if ($(".card-profile-stats-more-content").is(':hidden')) {
+        $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+    } else {
+        $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+    }
+    $(this).next('.card-profile-stats-more-content').slideToggle();
+});
+
